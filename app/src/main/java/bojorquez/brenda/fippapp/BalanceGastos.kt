3d00dbebe25208cd948cc8effe3ecd345a45f493
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-26 00:32:33 -0700

    Gráfica balance gastos lista

--- app/src/main/java/bojorquez/brenda/fippapp/BalanceGastos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/BalanceGastos.kt
@@ -1,4 +1,32 @@
 package bojorquez.brenda.fippapp
 
-class BalanceGastos {
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import kotlinx.android.synthetic.main.activity_balance_gastos.*
+
+class BalanceGastos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balance_gastos)
+
+        val graphView = findViewById<View>(R.id.idGraphView) as GraphView
+        val series = LineGraphSeries(
+            arrayOf<DataPoint>(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graphView.addSeries(series)
+
+    }
 }
