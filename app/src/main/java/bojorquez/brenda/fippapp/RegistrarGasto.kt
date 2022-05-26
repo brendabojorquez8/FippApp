d573c8b63acbc63ae94fd2d45d112d03f3e79455
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-26 13:45:10 -0700

    Registrar gasto e ingreso listo

--- app/src/main/java/bojorquez/brenda/fippapp/RegistrarGasto.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/RegistrarGasto.kt
@@ -1,4 +1,26 @@
 package bojorquez.brenda.fippapp
 
-class RegistrarGasto {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registrar_gasto.*
+
+class RegistrarGasto : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_gasto)
+
+        btn_dismiss.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_registrarGasto.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+    }
 }
