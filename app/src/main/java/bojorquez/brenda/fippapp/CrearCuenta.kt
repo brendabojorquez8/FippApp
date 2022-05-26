11ec148af0dd9a4403a1ff8fb54258a624b40133
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-25 19:42:21 -0700

    Front home y registro terminado

--- app/src/main/java/bojorquez/brenda/fippapp/CrearCuenta.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/CrearCuenta.kt
@@ -3,16 +3,20 @@ package bojorquez.brenda.fippapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.*
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_crear_cuenta.*
-import java.util.regex.Pattern
 
 class CrearCuenta : AppCompatActivity() {
 
     private lateinit var storage: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -33,55 +37,38 @@ class CrearCuenta : AppCompatActivity() {
     }
 
     private fun valida_registro(){
-
         var username: String = et_nombre_usuario.text.toString()
         var correo: String = et_correo_usuario.text.toString()
         var contra1: String = et_contras1.text.toString()
         var contra2: String = et_contras2.text.toString()
 
-        if(!correo.isNullOrBlank() && !contra1.isNullOrBlank() &&
-            !contra2.isNullOrBlank()){
-            if(contra1 == contra2 && contra1.length > 5){
-                if (!verifyEmail(correo)){
-                    registrarFirebase(correo, contra1)
-                    val intent: Intent = Intent(this, Home::class.java)
-                    startActivity(intent)
-                    this.finish()
-                }
+        auth.createUserWithEmailAndPassword(correo,contra1).addOnCompleteListener(this) {
+            // If register is successful, send them to home screen
+            if (it.isSuccessful) {
+                Log.d("FIREBASE", "User created successfully");
+                val user = hashMapOf(
+                    "name" to username,
+                    "email" to correo,
+                    "password" to contra1,
+                )
 
-            }else{
-                Toast.makeText(this, "Las contraseña no coinciden o es menor a 6 caracteres",
-                    Toast.LENGTH_SHORT).show()
+                val db = Firebase.firestore;
+
+                db.collection("users").document(correo)
+                    .set(user)
+                    .addOnSuccessListener {
+                        val intent = Intent(this, MainActivity::class.java);
+                        startActivity(intent);
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("FIREBASE", "User document creation failed: $e");
+                        Toast.makeText(this, "$e", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                // if an error occurs, show error message in Toast
+                Log.e("FIREBASE", "User creation failed: ${it.exception?.message}");
+                Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
             }
-
-        }else{
-            Toast.makeText(this, "Ingresar datos",
-                Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun registrarFirebase(email: String, password: String){
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    // Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    Toast.makeText(this,"  Authentication passed.",
-                        Toast.LENGTH_SHORT).show()
-                    // updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    // Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
-                }
-            }
-    }
-
-
-    private fun verifyEmail(email: String): Boolean{ //falso no pasa
-        return Pattern.compile("@\"^([\\w.-]+)@(\\[(\\d{1,3}\\.){3}|(?!hotmail|gmail|yahoo)(([a-zA-Z\\d-]+\\.)+))([a-zA-Z]{2,4}|\\d{1,3})(\\]?)\$\"").matcher(email).matches()
     }
 }
--- app/src/main/java/bojorquez/brenda/fippapp/Home.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/Home.kt
@@ -1,4 +1,17 @@
 package bojorquez.brenda.fippapp
 
-class Home {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+
+class Home : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
 }
