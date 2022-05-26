62d087a539a7b35593d7eff316b9f1d9ac2670a8
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-26 00:17:31 -0700

    Gráfica balance ingresos lista

--- app/src/main/java/bojorquez/brenda/fippapp/Home.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/Home.kt
@@ -1,5 +1,6 @@
 package bojorquez.brenda.fippapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home.*
@@ -12,6 +13,26 @@ class Home : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btn_Ingresos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_gastos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceGastos::class.java)
+            startActivity(intent)
+        }
+
+        btn_agregar_ingreso.setOnClickListener {
+            val intent: Intent = Intent(this, RegistrarIngreso::class.java)
+            startActivity(intent)
+        }
+
+        btn_agregar_gastos.setOnClickListener {
+            val intent: Intent = Intent(this, RegistrarGasto::class.java)
+            startActivity(intent)
+        }
 
     }
 }
