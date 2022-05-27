8c6ca76dd0640ba82973bbf2aa66bd37ec58546d
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-26 19:44:06 -0700

    Categorías gastos e ingresos listo

--- app/src/main/java/bojorquez/brenda/fippapp/CategoriasGastos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/CategoriasGastos.kt
@@ -1,4 +1,46 @@
 package bojorquez.brenda.fippapp
 
-class CategoriasGastos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_categorias_gastos.*
+
+class CategoriasGastos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorias_gastos)
+
+        btn_Ingresos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_clasificacion.setOnClickListener {
+            val intent: Intent = Intent(this, ClasificacionIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_clategorías.setOnClickListener {
+            val intent: Intent = Intent(this, CategoriasIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_fijos.setOnClickListener {
+            val intent: Intent = Intent(this, IngresosFijos::class.java)
+            startActivity(intent)
+        }
+
+        btn_registrarCatGasto.setOnClickListener {
+            val intent: Intent = Intent(this, RegistrarCategoriasGastos::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/bojorquez/brenda/fippapp/CategoriasIngresos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/CategoriasIngresos.kt
@@ -1,4 +1,46 @@
 package bojorquez.brenda.fippapp
 
-class CategoriasIngresos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_categorias_ingresos.*
+
+class CategoriasIngresos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorias_ingresos)
+
+        btn_gastos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceGastos::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_clasificacion.setOnClickListener {
+            val intent: Intent = Intent(this, ClasificacionIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_clategorías.setOnClickListener {
+            val intent: Intent = Intent(this, CategoriasIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_fijos.setOnClickListener {
+            val intent: Intent = Intent(this, IngresosFijos::class.java)
+            startActivity(intent)
+        }
+
+        btn_registrarCatIngreso.setOnClickListener {
+            val intent: Intent = Intent(this, RegistrarCategoriasIngresos::class.java)
+            startActivity(intent)
+        }
+    }
 }
