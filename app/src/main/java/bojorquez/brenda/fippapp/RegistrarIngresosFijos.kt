585cc1216f6d930c3d099f596e0e3a9b90ff038e
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-27 00:31:33 -0700

    Registrar ingresos y gastos fijos listo

--- app/src/main/java/bojorquez/brenda/fippapp/RegistrarIngresosFijos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/RegistrarIngresosFijos.kt
@@ -1,4 +1,26 @@
 package bojorquez.brenda.fippapp
 
-class RegistrarIngresosFijos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registros_ingresos_fijos.*
+
+class RegistrarIngresosFijos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registros_ingresos_fijos)
+
+        btn_dismiss.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_registrarCat.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/bojorquez/brenda/fippapp/RegistrosGastosFijos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/RegistrosGastosFijos.kt
@@ -1,4 +1,26 @@
 package bojorquez.brenda.fippapp
 
-class RegistrosGastosFijos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registrar_gastos_fijos.*
+
+class RegistrosGastosFijos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gastos_fijos)
+
+        btn_dismiss.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_registrarCat.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+    }
 }
