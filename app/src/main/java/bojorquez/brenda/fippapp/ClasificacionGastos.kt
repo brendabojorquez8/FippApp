40ec5796bbc75aff6afb5e6080281b842180d25d
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2022-05-26 19:13:29 -0700

    Clasificación gastos e ingresos listo

--- app/src/main/java/bojorquez/brenda/fippapp/ClasificacionGastos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/ClasificacionGastos.kt
@@ -1,4 +1,42 @@
 package bojorquez.brenda.fippapp
 
-class ClasificacionGastos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_clasificacion_gastos.*
+
+class ClasificacionGastos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clasificacion_gastos)
+
+        btn_Ingresos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+
+        btn_clasificacion.setOnClickListener {
+            val intent: Intent = Intent(this, ClasificacionGastos::class.java)
+            startActivity(intent)
+        }
+
+        btn_clategorías.setOnClickListener {
+            val intent: Intent = Intent(this, CategoriasGastos::class.java)
+            startActivity(intent)
+        }
+
+        btn_fijos.setOnClickListener {
+            val intent: Intent = Intent(this, GastosFijos::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/bojorquez/brenda/fippapp/ClasificacionIngresos.kt
+++ app/src/main/java/bojorquez/brenda/fippapp/ClasificacionIngresos.kt
@@ -1,4 +1,41 @@
 package bojorquez.brenda.fippapp
 
-class ClasificacionIngresos {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_clasificacion_ingresos.*
+
+class ClasificacionIngresos : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clasificacion_ingresos)
+
+        btn_gastos.setOnClickListener {
+            val intent: Intent = Intent(this, BalanceGastos::class.java)
+            startActivity(intent)
+        }
+
+        btn_home.setOnClickListener {
+            val intent: Intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        btn_clasificacion.setOnClickListener {
+            val intent: Intent = Intent(this, ClasificacionIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_clategorías.setOnClickListener {
+            val intent: Intent = Intent(this, CategoriasIngresos::class.java)
+            startActivity(intent)
+        }
+
+        btn_fijos.setOnClickListener {
+            val intent: Intent = Intent(this, IngresosFijos::class.java)
+            startActivity(intent)
+        }
+    }
 }
